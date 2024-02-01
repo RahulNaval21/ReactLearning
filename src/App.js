65823df2019a01d8d53be49878baa6e00ea29e48
RAHUL <rahulnaval157821@gmail.com>
2024-02-01 14:34:19 +0530

    Ch1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
+import Video from "./components/Video";
 
 function App() {
+  let obj = {
+    title: "React JS tutorial",
+    views: "999K",
+    time: "1 year ago",
+    channel: "Coder Dost",
+  };
   return (
     <div className="App">
-      <div className="App-header">Hello World</div>
+      <div>Videos</div>
+      <Video {...obj}></Video>
+      <Video title="Node JS tutorial" views="100K" time="1 month ago"></Video>
+      <Video
+        title="Mongo DB tutorial"
+        views="1M"
+        time="1 month ago"
+        channel="Coder Dost"
+      ></Video>
     </div>
   );
 }
